228bd74e70544f6dc3951f07763fd33c6718719b
Tom Hewitt <tommhewitt@gmail.com>
2019-11-01 09:29:11 +0000

    First commit

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour 
+{
+	GameObject[] buttons;
+
+	void Start()
+	{
+		buttons = GameObject.FindGameObjectsWithTag ("Button");
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			Application.Quit();
+		}
+	}
+
+    void OnMouseDown()
+    {
+		for (int i = 0; i < buttons.Length; i++) 
+		{
+			if (this.gameObject == buttons[i])
+			{
+				GetComponent<AudioSource> ().Play ();
+			} 
+			else 
+			{
+				buttons [i].GetComponent<AudioSource> ().Stop ();
+			}
+		}
+    }
+}
---
+++ Assets/Scripts/TapToStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapToStart : MonoBehaviour 
+{
+    void OnMouseDown()
+    {
+        Application.LoadLevel("Scene");
+    }
+}
