0790f8042f8dee810eedf4e9b14e42b0ac36cd40
Tom Hewitt <tommhewitt@gmail.com>
2019-11-27 17:00:27 +0000

    Rename some audio files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import easyIntense from './audio/Easy_Intense.mp3'
 import mediumIntense from './audio/Medium_Intense.mp3'
 import superIntense from './audio/Super_Intense.mp3'
+import clock from './audio/tick_tock_loop.mp3'
+
 import './App.css';
 
 class App extends React.Component {
@@ -9,8 +12,21 @@ class App extends React.Component {
 		return (
 			<div className='App'>
 				<ul>
+					<li><h3>Easy Intense</h3> <audio ref='easy' src={easyIntense} controls loop/></li>
+					<br></br>
+					
+					<li><h3>Medium Intense</h3> <audio ref='medium' src={mediumIntense} controls loop/></li>
+					<br></br>
+
 					<li><h3>Super Intense</h3> <audio ref='super' src={superIntense} controls loop/></li>
 					<br></br>
+
+					<li><h3>Clock</h3> <audio ref='clock' src={clock} controls loop/></li>
+					<br></br>
+
+					<li><h3>Medium Intense</h3> <audio ref='medium' src={mediumIntense} controls loop/></li>
+					<br></br>
+
 					<li><h3>Medium Intense</h3> <audio ref='medium' src={mediumIntense} controls loop/></li>
 					<br></br>
 				</ul>
