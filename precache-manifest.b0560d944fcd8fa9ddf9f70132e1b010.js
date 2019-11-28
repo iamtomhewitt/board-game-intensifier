7297bbc10621368d1586e68d410557b834899d0f
Tom Hewitt <tom.hewitt@williamhill.com>
2019-11-28 10:17:57 +0000

    Updates

---
+++ precache-manifest.b0560d944fcd8fa9ddf9f70132e1b010.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a6cad3af571d5818d191040ba2fa80",
+    "url": "/board-game-intensifier/index.html"
+  },
+  {
+    "revision": "c9931fdfcd9da4557155",
+    "url": "/board-game-intensifier/static/css/main.555d3e52.chunk.css"
+  },
+  {
+    "revision": "dbe2bfc3a1c2443613a8",
+    "url": "/board-game-intensifier/static/js/2.d7866121.chunk.js"
+  },
+  {
+    "revision": "c9931fdfcd9da4557155",
+    "url": "/board-game-intensifier/static/js/main.a764f332.chunk.js"
+  },
+  {
+    "revision": "41841a24b4cd14e892c4",
+    "url": "/board-game-intensifier/static/js/runtime-main.8ae6c97b.js"
+  },
+  {
+    "revision": "cc4c01dc666be7988f5886c852cff0b7",
+    "url": "/board-game-intensifier/static/media/are-you-sure.cc4c01dc.mp3"
+  },
+  {
+    "revision": "6b68adc08df964560db9d263ce5fa74b",
+    "url": "/board-game-intensifier/static/media/chris-tarrent.6b68adc0.mp3"
+  },
+  {
+    "revision": "24abf49652fdf176c81a1708054de1e4",
+    "url": "/board-game-intensifier/static/media/clock-telephone.24abf496.mp3"
+  },
+  {
+    "revision": "18c53f70c7e176cc1daaf56a58bc9a58",
+    "url": "/board-game-intensifier/static/media/correct-4.18c53f70.mp3"
+  },
+  {
+    "revision": "73621f567828af9746144ffb3496df4f",
+    "url": "/board-game-intensifier/static/media/intro-5.73621f56.mp3"
+  },
+  {
+    "revision": "5693e71c7676a20f02ca3838f26ca82a",
+    "url": "/board-game-intensifier/static/media/music-easy.5693e71c.mp3"
+  },
+  {
+    "revision": "9f40df4fb2e0fefacd62c5181aef5064",
+    "url": "/board-game-intensifier/static/media/music-expert.9f40df4f.mp3"
+  },
+  {
+    "revision": "484de5f74d4f0d8576d06c0799f73b70",
+    "url": "/board-game-intensifier/static/media/music-hard.484de5f7.mp3"
+  },
+  {
+    "revision": "917ca99b99a61ea42c83a5e71dcbf90f",
+    "url": "/board-game-intensifier/static/media/music-medium.917ca99b.mp3"
+  },
+  {
+    "revision": "20864f8d5a68908868c5936723aaa8cd",
+    "url": "/board-game-intensifier/static/media/showdown-1.20864f8d.mp3"
+  },
+  {
+    "revision": "d02d38e35c049f01d31c88ef882973df",
+    "url": "/board-game-intensifier/static/media/wrong-answer-2.d02d38e3.mp3"
+  }
+]);
